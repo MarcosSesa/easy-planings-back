5b44f4b7cc0edd4f89977cc2b98b4e5b548cf3d6
MarcosSesa <marcoxxtee@gmail.com>
2026-02-16 20:19:03 +0100

    chore: init project

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando 🚀" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
