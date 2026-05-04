aebbbe95552a7e44e71a2df98666742b3eb804e9
MarcosSesa <marcoxxtee@gmail.com>
2026-05-04 18:33:49 +0200

    feat: update datasource URL in prisma configuration for local development

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
         path: "prisma/migrations",
     },
     datasource: {
-        url: process.env.DATABASE_URL ?? "postgresql://dummy:dummy@localhost:5432/dummy",
+        url: process.env.DATABASE_URL ?? "postgresql://dummy:dummy@localhost:9992/dummy",
     },
 });
